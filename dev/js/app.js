c3bb1bfe965f0b06a800bd0d300a71188256d132
Fayek Helmi <fayek.helmi@gmail.com>
2017-06-19 22:46:59 -0400

    created scrolling functionality that fixes navbar to top and shows titlebar when scrolled

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,4 +1,30 @@
 $(document).ready(function(){
-  console.log("jQuery is ready");
+ var navbarPosition = 0; 
+ var isFixed = false;
+
+ 
+ 
+$(window).on('scroll', function(navbarPosition){
+  navbarPosition = ($(".js-navbar-marker").offset().top - $(window).scrollTop());
   
+  if (navbarPosition < 0 && !isFixed){
+    isFixed = true;
+    $(".navbar-wrapper").addClass("js-fixedPos");
+    $('.scrolled-title').css({height: '50px'});
+  } 
+  if (navbarPosition > 0 && navbarPosition) {
+      isFixed = false;
+      $(".navbar-wrapper").removeClass("js-fixedPos");
+      $('.scrolled-title').css({height: '0'});
+      
+    }
+  
+ 
 });
+
+});
+
+
+
+// waypoints:
+
